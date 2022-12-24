e7c2222829e58ce0497e2e55bc5e27fc92c0befb
Diani Chandra <chandradiani@gmail.com>
2022-12-24 20:58:11 +0800

    view offer history complete

--- resources/views/volunteer/menu.blade.php
+++ resources/views/volunteer/menu.blade.php
@@ -16,15 +16,15 @@
 <!-- Nav Item - Add School -->
 <li class="nav-item">
     <a class="nav-link collapsed" href="/dashboard">
-        <i class="fas fa-fw fa-solid fa-school"></i>
-        <span>View Request History</span>
+        <i class="fas fa-fw fa-solid fa-calendar"></i>
+        <span>View Request Available</span>
     </a>
 </li>
 
 <li class="nav-item">
     <a class="nav-link collapsed" href="{{route('offer.list')}}">
-        <i class="fas fa-fw fa-solid fa-school"></i>
-        <span>View Offer</span>
+        <i class="fas fa-fw fa-solid fa-handshake"></i>
+        <span>View Offer History</span>
     </a>
 </li>
 
